f585418a41fc230949d9614e837a4293c2ea73bb
Argha0004 <arghadalal354@gmail.com>
2024-09-03 19:59:56 +0530

    Getting Input

---
+++ lecture_2.py
@@ -0,0 +1,12 @@
+""" Getting Input """
+"""
+'()' -> we have these parenthesis, we're going to say we'ar calling or executing that functions.
+'+' -> we're concatenating or combining the string with another string by + sign 
+"""
+# name = input('what is your name? \n=>')
+# print('Hi,' + name) # dynamically print
+
+
+name = input('What is your name? \n=> ')
+fav_color = input('What is your fav color? \n=> ')
+print(name + ' fav color is ' + fav_color) # dynamically print
