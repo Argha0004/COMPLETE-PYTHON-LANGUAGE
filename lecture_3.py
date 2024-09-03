5d9444806a3e8162b0e3cb3dd8a8e16316bf64d4
Argha0004 <arghadalal354@gmail.com>
2024-09-03 20:54:45 +0530

    Type Conversion like int, string, float and bollean

---
+++ lecture_3.py
@@ -0,0 +1,14 @@
+""" Type Conversion """
+'''
+Calculating the age to birthday
+'''
+
+# birth_year = input('Birth Year: ')
+# print(type(birth_year))
+# age = 2024 - int(birth_year)
+# print(type(age))
+# print(age)
+
+wight_lbs = input('Wight in pounds: ')
+wight_kg = 0.45 * float(wight_lbs)
+print('Weight in kilograms: ', wight_kg)
