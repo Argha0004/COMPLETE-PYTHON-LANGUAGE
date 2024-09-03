6e18309fb7eb8e160e70c5ec749ea332634ec772
Argha0004 <arghadalal354@gmail.com>
2024-09-03 19:34:42 +0530

    Variables

---
+++ lecture_1.py
@@ -0,0 +1,15 @@
+# Variables---->>>>>
+"""
+Python is case sensitive language, which means it's sensitive to lower case and upper case letters. Here, "False and True" special keywords in the language 
+
+"""
+# price = 10
+# rating = 4.9
+# name = 'argha'
+# is_published = False
+# print(price)
+
+name = "Jhon Smith"
+age = 20
+is_newPatient = True
+print(name, age, is_newPatient)
