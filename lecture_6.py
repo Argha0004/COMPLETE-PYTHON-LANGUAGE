e1b54f4e58f16f2ba23ada8c50fe2e6f9df0aa3f
Argha0004 <arghadalal354@gmail.com>
2024-09-07 09:37:11 +0530

    String Method complete

---
+++ lecture_6.py
@@ -0,0 +1,21 @@
+""" String Methods """
+'''
+Function and method definitions are different. 
+
+print(), len() -> Functions
+
+upper(), lower() -> Method in string, this are general
+
+searching character's index values then use 'find()' method,
+
+searching character's boolean expression then use 'in'
+'''
+course = "Python For Beginners"
+# print(len(course))
+print(course.upper())
+print(course.lower())
+print(course)
+print(course.find('s')) 
+print(course.replace('Beginners', 'Students'))
+print('Python' in course)
+print(course.title() )
