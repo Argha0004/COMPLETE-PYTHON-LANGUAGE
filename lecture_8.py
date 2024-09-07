51040d4cd16db117d1da582509aca00953524cae
Argha0004 <arghadalal354@gmail.com>
2024-09-07 23:47:43 +0530

    Math moudles

---
+++ lecture_8.py
@@ -0,0 +1,12 @@
+""" Math Functions """
+'''
+'abs()' -> use for exchange negative value to positive value
+'''
+
+import math
+
+x = 2.9
+print(math.ceil(x))
+print(math.floor(x))
+# print(round(x))
+# print(abs(-2.9)) 
