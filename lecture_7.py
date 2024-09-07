80e6c8e605efacbbf73a7152ecb64d160c457dce
Argha0004 <arghadalal354@gmail.com>
2024-09-07 09:59:13 +0530

    Arithmetic Operations or Operator Precedence

---
+++ lecture_7.py
@@ -0,0 +1,26 @@
+""" Arithmetic Operations"""
+'''
+Types of arithmetic operators -> /, //, %, **
+
+Augmented assignment operators -> +=, -=,
+'''
+
+print(10 // 3)
+
+x = 3
+x += 10
+print(x)
+
+""" ************************************************************ """
+""" Operator Precedence """
+'''
+Order of precedence ->
+* parenthesis
+* exponentiation 2 ** 3
+* multiplication or division
+* addition or subtraction
+
+'''
+
+x = (3 +2) * 10 - 3
+print(x)
