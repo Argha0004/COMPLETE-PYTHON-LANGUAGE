9fb2ea7ceb5585a0899721ffa86242a28b77f772
Argha0004 <arghadalal354@gmail.com>
2024-09-08 23:36:28 +0530

    Concept of While Loops

---
+++ lecture_11.py
@@ -0,0 +1,11 @@
+""" While Loops """
+'''
+we use while loops to execute a block of code multiple times and there are often usefil in interactive programs and games.
+
+'''
+
+i = 0
+while i <= 5:
+    print("*" * i)
+    i += 1
+print("DONE")
